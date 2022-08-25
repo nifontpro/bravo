fec42a3f960e452dbbd020125b5b3028791540cb
Nifont <nifontbus@yandex.ru>
2022-08-25 15:25:08 +0300

    4.22 +ref

--- app/core/presenter/Layout/Layout.tsx
+++ app/core/presenter/Layout/Layout.tsx
@@ -4,15 +4,21 @@ import styles from '@/core/presenter/Layout/Layout.module.scss'
 import Navigation from '@/core/presenter/Layout/Navigation/Navigation'
 import Sidebar from "@/core/presenter/Layout/Sidebar/Sidebar";
 import AuthComponent from "@/core/providers/AuthProvider/AuthComponent";
+import {useAuthState} from "@/auth/data/auth.slice";
 
 const Layout: FC<PropsWithChildren> = ({children}) => {
+
+	const {user} = useAuthState()
+
 	return (
 		<div className={styles.layout}>
 			<Navigation/>
 			<div className={styles.center}>{children}</div>
-			<AuthComponent minRole="user">
-				<Sidebar/>
-			</AuthComponent>
+			{user ?
+				< Sidebar/>
+				:
+				<div className="text-2xl py-5 items-center">Добро пожаловать!</div>
+			}
 		</div>
 	)
 }
