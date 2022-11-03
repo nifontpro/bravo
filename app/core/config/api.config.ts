20d1478fde9649eeef7f63dd3f51040d72ad0376
Nifont <nifontbus@yandex.ru>
2022-11-03 21:59:01 +0300

    6.22 +url api

--- app/core/config/api.config.ts
+++ app/core/config/api.config.ts
@@ -1,7 +1,7 @@
 // export const API_SERVER_URL = `${process.env.APP_SERVER_URL}:8080/api`
 // export const SOCKET_URL = `ws://${process.env.APP_SERVER_URL}:8080/chat`
-export const API_SERVER_URL = `http://nmedals.ru:8080/api`
-export const SOCKET_URL = `ws://http://nmedals.ru:8080/api/chat`
+export const API_SERVER_URL = `http://nmedalist.ru:8080/api`
+export const SOCKET_URL = `ws://http://nmedalist.ru:8080/api/chat`
 export const REFRESH_TOKEN_LIFE = 30 // Время жизни локальной куки в днях для RT.
 
 export const getAuthUrl = (string: string) => `/auth${string}`
