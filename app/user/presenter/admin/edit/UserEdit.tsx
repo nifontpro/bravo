bcfb4ff2acb38bd8f67e9e97ab592dd7d74ddbf6
Skater Punisher <skaterpunisher@gmail.com>
2022-11-15 13:36:49 +0300

    dosn't work

--- app/user/presenter/admin/edit/UserEdit.tsx
+++ app/user/presenter/admin/edit/UserEdit.tsx
@@ -1,116 +1,234 @@
-import {ChangeEvent, FC, useEffect, useState} from 'react'
-import {useForm} from "react-hook-form";
-import Meta from "@/core/utils/meta/Meta";
-import AdminNavigation from "@/admin/presenter/admin-navigation/AdminNavigation";
-import Heading from "@/core/presenter/ui/heading/Heading";
-import SkeletonLoader from "@/core/presenter/ui/sceleton-loader/SkeletonLoader";
-import formStyles from "@/core/presenter/ui/form/admin-form.module.scss"
-import styles from "@/core/presenter/ui/form/form.module.scss"
-import Field from "@/core/presenter/ui/form/Field";
-import Button from "@/core/presenter/ui/form/Button";
-import cn from "classnames";
-import {useUserEdit} from "@/user/presenter/admin/edit/useUserEdit";
-import {IUserEditInput} from "@/user/presenter/admin/edit/user-edit.type";
-import {ImageDefault} from "@/core/presenter/ui/icons/ImageDefault";
+import { ChangeEvent, FC, useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import Meta from '@/core/utils/meta/Meta';
+import AdminNavigation from '@/admin/presenter/admin-navigation/AdminNavigation';
+import Heading from '@/core/presenter/ui/heading/Heading';
+import SkeletonLoader from '@/core/presenter/ui/sceleton-loader/SkeletonLoader';
+import formStyles from '@/core/presenter/ui/form/admin-form.module.scss';
+import styles from '@/core/presenter/ui/form/form.module.scss';
+import Field from '@/core/presenter/ui/form/Field';
+import Button from '@/core/presenter/ui/form/Button';
+import cn from 'classnames';
+import { useUserEdit } from '@/user/presenter/admin/edit/useUserEdit';
+import { IUserEditInput } from '@/user/presenter/admin/edit/user-edit.type';
+import { ImageDefault } from '@/core/presenter/ui/icons/ImageDefault';
+import { ImageRef } from '@/core/model/image.types';
+import uniqid from 'uniqid';
 
 const UserEdit: FC = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    setValue,
+  } = useForm<IUserEditInput>({
+    mode: 'onChange',
+  });
 
-	const {handleSubmit, register, formState: {errors}, setValue} =
-		useForm<IUserEditInput>({
-			mode: 'onChange'
-		})
+  // Слайдер фото для фото профиля НАЧАЛО
 
-	const {user, isLoading, onSubmit} = useUserEdit(setValue)
+//   const exampleImages: ImageRef[] = [
+//     {
+//       imageUrl:
+//         'https://fanibani.ru/images/wp-content/uploads/2021/01/image181-15-960x720.jpg',
+//       imageKey: '1-1-1',
+//     },
+//     {
+//       imageUrl: 'https://coolsen.ru/wp-content/uploads/2021/06/15-8.jpg',
+//       imageKey: '2-2-2',
+//     },
+//     {
+//       imageUrl:
+//         'https://pixelbox.ru/wp-content/uploads/2020/12/ava-vk-cats-76.jpg',
+//       imageKey: '2-2-2',
+//     },
+//   ];
 
-	const [image, setImage] = useState<string | undefined>(undefined)
+  const [imagesArtem, setImageArtem] = useState<ImageRef[] | undefined>(
+    undefined
+  );
+  const [numberUrl, setNumberUrl] = useState<number>(0);
 
-	const onImageChange = (event: ChangeEvent<HTMLInputElement>) => {
-		if (event.target.files && event.target.files[0]) {
-			setImage(URL.createObjectURL(event.target.files[0]));
-		}
-	}
+  const handleNext = () => {
+    if (imagesArtem !== undefined) {
+      if (numberUrl < imagesArtem.length - 1) {
+        setNumberUrl((prev) => prev + 1);
+      }
+      if (numberUrl == imagesArtem.length - 1) {
+        setNumberUrl(0);
+      }
+    }
+  };
 
-	useEffect(() => {
-		setImage(user?.imageUrl)
-	}, [user])
+  const handlePrev = () => {
+    if (imagesArtem !== undefined) {
+      if (numberUrl > 0) {
+        setNumberUrl((prev) => prev - 1);
+      }
+      if (numberUrl == 0) {
+        setNumberUrl(imagesArtem.length - 1);
+      }
+    }
+  };
 
-	return <Meta title="Редактирование профиля сотрудника">
-		<AdminNavigation/>
-		<Heading title="Редактирование профиля сотрудника"/>
-		<form onSubmit={handleSubmit(onSubmit)} className={formStyles.form}>
-			{isLoading ? <SkeletonLoader count={3}/> : <>
+  const handleRemove = () => {
+    if (imagesArtem !== undefined) {
+      if (imagesArtem.length == 1) {
+        console.log('Нельзя удалить последнее изображение');
+      } else {
+        imagesArtem.splice(numberUrl, 1);
+        setImageArtem(imagesArtem);
+        setNumberUrl(0);
+      }
+    }
+  };
 
-				<div className={cn(styles.field, styles.uploadField)}>
-					<div className={styles.uploadFlex}>
-						<label>
-							<ImageDefault
-								src={image} width={150} height={150} alt="preview image" objectFit="cover"
-								className="rounded-full"
-							/>
-							<div>
-								<span>Выберите новое изображение</span>
-								<input type="file" {...register("file")} onChange={onImageChange}/>
-							</div>
-						</label>
-					</div>
-				</div>
+  const onImageAdd = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      let obj: ImageRef = {
+        imageUrl: URL.createObjectURL(event.target.files[0]),
+        imageKey: uniqid(),
+      };
+      if (imagesArtem !== undefined) {
+        let arr = [...imagesArtem];
+        arr?.unshift(obj);
+        setImageArtem(arr);
+        setNumberUrl(0);
+      }
+    }
+  };
+  // Слайдер фото для фото профиля КОНЕЦ
 
-				<div className={formStyles.fields}>
-					<Field
-						{...register('lastname', {required: 'Фамилия необходима!'})}
-						placeholder='Фамилия'
-						error={errors.lastname}
-						style={{width: '31%'}}
-					/>
+  const { user, isLoading, onSubmit } = useUserEdit(setValue);
+//   const images = { ...user };
 
-					<Field
-						{...register('name', {required: 'Имя необходимо!'})}
-						placeholder='Имя'
-						error={errors.name}
-						style={{width: '31%'}}
-					/>
+//   const [image, setImage] = useState<string | undefined>(undefined);
 
-					<Field
-						{...register('patronymic')}
-						placeholder='Отчество'
-						error={errors.patronymic}
-						style={{width: '31%'}}
-					/>
+//   const onImageChange = (event: ChangeEvent<HTMLInputElement>) => {
+//     if (event.target.files && event.target.files[0]) {
+//       setImage(URL.createObjectURL(event.target.files[0]));
+//     }
+//   };
 
-					<Field
-						{...register('login')}
-						placeholder='Логин (Уникальный)'
-						error={errors.login}
-						style={{width: '31%'}}
-					/>
+//   useEffect(() => {
+//     setImage(user?.imageUrl);
+//   }, [user]);
 
-					<Field
-						{...register('password')}
-						placeholder='Пароль'
-						error={errors.password}
-						style={{width: '31%'}}
-					/>
+  useEffect(() => {
+    setImageArtem(user?.images);
+    // console.log('useEffect')
+  }, [user]);
 
-					<Field
-						{...register('email')}
-						placeholder='Email'
-						error={errors.email}
-						style={{width: '31%'}}
-					/>
+  console.log(user)
 
-					<div className={formStyles.checkbox}>
-						Член номинационной комиссии
-						<input
-							type="checkbox"
-							{...register('isMNC')}
-						/>
-					</div>
+  return (
+    <Meta title='Редактирование профиля сотрудника'>
+      <AdminNavigation />
+      <Heading title='Редактирование профиля сотрудника' />
 
-				</div>
-				<Button className="">Обновить</Button>
-			</>}
-		</form>
-	</Meta>
-}
+      <form onSubmit={handleSubmit(onSubmit)} className={formStyles.form}>
+        {isLoading ? (
+          <SkeletonLoader count={3} />
+        ) : (
+          <>
+            <div className='flex justify-center items-center my-10'>
+              <div className='mr-10 relative'>
+                {imagesArtem && <ImageDefault
+				{...register('images', {required: 'Ajn необходимо!'})}
+                  src={imagesArtem[numberUrl].imageUrl}
+                  width={150}
+                  height={150}
+                  alt='preview image'
+                  objectFit='cover'
+                  className='rounded-[10px]'
+                /> }
 
-export default UserEdit
+                <div
+                  onClick={handleNext}
+                  className='absolute right-0 top-[45%] cursor-pointer bg-black text-white'
+                >
+                  Next
+                </div>
+                <div
+                  onClick={handlePrev}
+                  className='absolute left-0 top-[45%] cursor-pointer bg-black text-white'
+                >
+                  Prev
+                </div>
+                <div
+                  onClick={handleRemove}
+                  className='absolute right-0 top-0 cursor-pointer bg-black text-white'
+                >
+                  Remove
+                </div>
+              </div>
+
+              <div className={cn(styles.field, styles.uploadField)}>
+                <div className={styles.uploadFlex}>
+                  <label>
+                    <div>
+                      <span>Добавить изображение</span>
+                      <input type='file' onChange={onImageAdd} />
+                    </div>
+                  </label>
+                </div>
+              </div>
+            </div>
+
+            <div className={formStyles.fields}>
+              <Field
+                {...register('lastname', { required: 'Фамилия необходима!' })}
+                placeholder='Фамилия'
+                error={errors.lastname}
+                style={{ width: '31%' }}
+              />
+
+              <Field
+                {...register('name', { required: 'Имя необходимо!' })}
+                placeholder='Имя'
+                error={errors.name}
+                style={{ width: '31%' }}
+              />
+
+              <Field
+                {...register('patronymic')}
+                placeholder='Отчество'
+                error={errors.patronymic}
+                style={{ width: '31%' }}
+              />
+
+              <Field
+                {...register('login')}
+                placeholder='Логин (Уникальный)'
+                error={errors.login}
+                style={{ width: '31%' }}
+              />
+
+              <Field
+                {...register('password')}
+                placeholder='Пароль'
+                error={errors.password}
+                style={{ width: '31%' }}
+              />
+
+              <Field
+                {...register('email')}
+                placeholder='Email'
+                error={errors.email}
+                style={{ width: '31%' }}
+              />
+
+              <div className={formStyles.checkbox}>
+                Член номинационной комиссии
+                <input type='checkbox' {...register('isMNC')} />
+              </div>
+            </div>
+            <Button className=''>Обновить</Button>
+          </>
+        )}
+      </form>
+    </Meta>
+  );
+};
+
+export default UserEdit;
