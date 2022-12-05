6d30862e2882be27d2760c03ae166e357b80014a
Nifont <nifontbus@yandex.ru>
2022-12-06 00:21:13 +0300

    7.03 Исправил ошибку с установкой 1 компании у владельца по умолчанию

--- app/auth/presenter/useSetAuthData.ts
+++ app/auth/presenter/useSetAuthData.ts
@@ -4,6 +4,8 @@ import {useMemo} from "react";
 import {IAuthResponse} from "@/auth/model/auth.types";
 import {ICompany} from "@/company/model/company.types";
 import {getCompanyByStorage} from "@/auth/data/auth.helper";
+import {useDispatch} from "react-redux";
+import {companyActions} from "@/company/data/company.slice";
 
 /**
  * Хук устанавливает текущую компанию и отдел при входе пользователя
@@ -13,7 +15,7 @@ export const useSetAuthData = () => {
     const [setCompany] = companyApi.useSetByIdMutation()
     const [setDepartment] = departmentApi.useSetByIdMutation()
     const [getOwnerCompanies] = companyApi.useGetByOwnerParamMutation()
-    // const dispatch = useDispatch()
+    const dispatch = useDispatch();
 
     return useMemo(() => {
 
@@ -34,10 +36,12 @@ export const useSetAuthData = () => {
                 if (user.role == "owner" && getCompanyByStorage() == null) {
 
                     await getOwnerCompanies().unwrap().then(async (companies: ICompany[]) => {
-                        console.log('Companies:')
+                        console.log('COMPANIES:')
                         console.log(companies)
                         if (companies.length > 0) {
-                            setCompany(companies[0].id)
+                            // setCompany(companies[0].id)
+                            console.log("SET COMPANY: " + companies[0].name)
+                            dispatch(companyActions.setState(companies[0]));
                         }
                     })
                 }
@@ -45,6 +49,6 @@ export const useSetAuthData = () => {
                 // dispatch(authSlice.actions.setWs()) // Открываем сокет
             }
             return {setAuthData}
-        }, [getOwnerCompanies, setCompany, setDepartment]
+        }, [dispatch, getOwnerCompanies, setCompany, setDepartment]
     )
 }
