c59c414ec81048bce1608b58495297bd9446a921
Zykov Artem <skaterpunisher@gmail.com>
2022-12-24 18:04:43 +0300

    fix UserModalWindow and create useUserModalWindow

--- app/core/presenter/Layout/Header/UserModalWindow/UserModalWindow.tsx
+++ app/core/presenter/Layout/Header/UserModalWindow/UserModalWindow.tsx
@@ -7,20 +7,8 @@ import EditIcon from '@/core/presenter/images/editProfile.svg';
 import ExitIcon from '@/core/presenter/images/exit.svg';
 import ChangePasswordIcon from '@/core/presenter/images/changePassword.svg';
 import P from '@/core/presenter/ui/P/P';
-import { useRouter } from 'next/router';
-import {
-  getUserEditPasswordUrl,
-  getUserEditUrl,
-  getUserUrl,
-} from '@/core/config/api.config';
-import { useDispatch } from 'react-redux';
-import { authActions } from '@/auth/data/auth.slice';
-import { companyActions } from '@/company/data/company.slice';
-import { departmentActions } from '@/department/data/department.slice';
-import { companyApi } from '@/company/data/company.api';
-import { departmentApi } from '@/department/data/department.api';
-import { userApi } from '@/user/data/user.api';
 import { ForwardedRef, forwardRef } from 'react';
+import { useUserModalWindow } from './useUserModalWindow';
 
 const UserModalWindow = forwardRef(
   (
@@ -33,34 +21,12 @@ const UserModalWindow = forwardRef(
     }: UserModalWindowProps,
     ref: ForwardedRef<HTMLDivElement>
   ): JSX.Element => {
-    const { push } = useRouter();
-    const dispatch = useDispatch();
-
-    const handleClickProfile = () => {
-      push(getUserUrl(`/${user?.id}`));
-      setVisibleModal(false);
-    };
-
-    const handleClickEditProfile = () => {
-      push(getUserEditUrl(`/${user?.id}`));
-      setVisibleModal(false);
-    };
-
-    const handleClickEditPassword = () => {
-      push(getUserEditPasswordUrl(`/${user?.id}`));
-      setVisibleModal(false);
-    };
-
-    const handleLogout = () => {
-      dispatch(authActions.logout());
-      dispatch(companyActions.clear());
-      dispatch(departmentActions.clear());
-      dispatch(companyApi.util.resetApiState());
-      dispatch(departmentApi.util.resetApiState());
-      dispatch(userApi.util.resetApiState());
-      // dispatch(userApi.util.invalidateTags(['User']))
-      push('/auth');
-    };
+    const {
+      handleClickProfile,
+      handleClickEditProfile,
+      handleClickEditPassword,
+      handleLogout,
+    } = useUserModalWindow(setVisibleModal, user);
 
     return (
       <div
---
+++ app/core/presenter/Layout/Header/UserModalWindow/useUserModalWindow.ts
@@ -0,0 +1,55 @@
+import { authActions } from '@/auth/data/auth.slice';
+import { companyApi } from '@/company/data/company.api';
+import { companyActions } from '@/company/data/company.slice';
+import {
+  getUserEditPasswordUrl,
+  getUserEditUrl,
+  getUserUrl,
+} from '@/core/config/api.config';
+import { departmentApi } from '@/department/data/department.api';
+import { departmentActions } from '@/department/data/department.slice';
+import { userApi } from '@/user/data/user.api';
+import { IUser } from '@/user/model/user.types';
+import { useRouter } from 'next/router';
+import { Dispatch, SetStateAction } from 'react';
+import { useDispatch } from 'react-redux';
+
+export const useUserModalWindow = (
+  setVisibleModal: Dispatch<SetStateAction<boolean>>,
+  user: IUser | undefined
+) => {
+  const { push } = useRouter();
+  const dispatch = useDispatch();
+
+  const handleClickProfile = () => {
+    push(getUserUrl(`/${user?.id}`));
+    setVisibleModal(false);
+  };
+
+  const handleClickEditProfile = () => {
+    push(getUserEditUrl(`/${user?.id}`));
+    setVisibleModal(false);
+  };
+
+  const handleClickEditPassword = () => {
+    push(getUserEditPasswordUrl(`/${user?.id}`));
+    setVisibleModal(false);
+  };
+
+  const handleLogout = () => {
+    dispatch(authActions.logout());
+    dispatch(companyActions.clear());
+    dispatch(departmentActions.clear());
+    dispatch(companyApi.util.resetApiState());
+    dispatch(departmentApi.util.resetApiState());
+    dispatch(userApi.util.resetApiState());
+    push('/auth');
+  };
+
+  return {
+    handleClickProfile,
+    handleClickEditProfile,
+    handleClickEditPassword,
+    handleLogout,
+  };
+};
