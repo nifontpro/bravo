0be0c5c2d9440ac1ad4d0e6a80845d389d15275f
Nifont <nifontbus@yandex.ru>
2022-12-29 18:30:21 +0300

    +activity api

---
+++ app/activity/data/activity.api.ts
@@ -0,0 +1,27 @@
+import {queryWithReauth} from "@/core/data/base.api";
+import {createApi} from "@reduxjs/toolkit/dist/query/react";
+import {getActivityUrl} from "@/core/config/api.config";
+import {IGetActivitiesRequest} from "@/activity/model/request.types";
+import {IActivity} from "@/activity/model/activity.types";
+
+export const activityApi = createApi({
+	reducerPath: 'activityApi',
+	baseQuery: queryWithReauth,
+	tagTypes: ['Activity'],
+	endpoints: (build) => ({
+
+		/**
+		 * Получить активность в компании
+		 * @param [IGetActivitiesRequest] - см. расшифровку тела запроса в интерфейсе
+		 */
+		getAwardCount: build.query<IActivity[], IGetActivitiesRequest>({
+			query: (request) => ({
+				method: 'POST',
+				url: getActivityUrl("/get_c"),
+				body: request
+			}),
+			providesTags: ['Activity']
+		}),
+
+	})
+})
---
+++ app/activity/model/request.types.ts
@@ -0,0 +1,12 @@
+export interface IGetActivitiesRequest {
+	companyId: string
+
+	startDate?: number // ограничения по дате событий
+	endDate?: number
+
+	filter?: string // фильтрация по фамилии/имени сотрудника или наименованию отдела
+	sort?: number // направление сортировки по дате 1 / -1 (по умолчанию -1 - по убыванию)
+
+	page?: number // номер страницы, начиная с 0
+	pageSize?: number // размер страницы, если эти 2 параметра не указаны, то все элементы
+}
