5683d69956f566c9378a2da83b401e43f7f0bbd1
Zykov Artem <skaterpunisher@gmail.com>
2023-01-12 16:55:49 +0300

    feat Header Notification and UserLogo

--- app/core/presenter/Layout/Header/Header.tsx
+++ app/core/presenter/Layout/Header/Header.tsx
@@ -2,48 +2,18 @@ import styles from './Header.module.scss';
 import cn from 'classnames';
 import { HeaderProps } from './Header.props';
 import LogoIcon from '@/core/presenter/images/logo.svg';
-import NotificationIcon from '@/core/presenter/images/notification.svg';
 import Link from 'next/link';
 import Search from '../../ui/Search/Search';
 import { useAuthState } from '@/auth/data/auth.slice';
-import { ImageDefault } from '../../ui/icons/ImageDefault';
-import { useRef, useState } from 'react';
-import useOutsideClick from '@/core/hooks/useOutsideClick';
-import UserPanelModalWindow from './UserPanelModalWindow/UserPanelModalWindow';
-import NotificationModalWindow from './NotificationModalWindow/NotificationModalWindow';
 import { messageApi } from 'message/data/message.api';
-import P from '../../ui/P/P';
+import UserLogo from './UserLogo/UserLogo';
+import Notification from './Notification/Notification';
 
 const Header = ({ className, ...props }: HeaderProps): JSX.Element => {
   const { user } = useAuthState();
   const { data: allMessage } = messageApi.useGetByUserQuery(user?.id || '', {
     skip: !user?.id,
   });
-
-  const [visibleModal, setVisibleModal] = useState<boolean>(false);
-  const [visibleNotification, setVisibleNotification] =
-    useState<boolean>(false);
-
-  //Закрытие модального окна уведомлений нажатием вне
-  const refNotification = useRef(null);
-  const refOpenNotification = useRef(null);
-  const handleClickOutsideNotification = () => {
-    setVisibleNotification(false);
-  };
-  useOutsideClick(
-    refNotification,
-    refOpenNotification,
-    handleClickOutsideNotification,
-    visibleNotification
-  );
-
-  //Закрытие модального окна пользователя нажатием вне
-  const ref = useRef(null);
-  const refOpen = useRef(null);
-  const handleClickOutside = () => {
-    setVisibleModal(false);
-  };
-  useOutsideClick(ref, refOpen, handleClickOutside, visibleModal);
 
   return (
     <header className={cn(className, styles.header)} {...props}>
@@ -60,54 +30,9 @@ const Header = ({ className, ...props }: HeaderProps): JSX.Element => {
         className={styles.search}
       />
       <div className={styles.user}>
-        <div
-          className={styles.userComponent}
-          ref={refOpenNotification}
-          onClick={() => setVisibleNotification(!visibleNotification)}
-        >
-          <NotificationIcon className={styles.notification} />
-          {allMessage &&
-            allMessage.filter((item) => item.read == false).length > 0 && (
-              <>
-                <div className={styles.notificationCircle}></div>
-                <P
-                  size='xs'
-                  fontstyle='thin'
-                  className={styles.notificationCount}
-                >
-                  {allMessage?.filter((item) => item.read == false).length}
-                </P>
-              </>
-            )}
-        </div>
-
-        <div
-          className={styles.userImg}
-          onClick={() => setVisibleModal(!visibleModal)}
-          ref={refOpen}
-        >
-          <ImageDefault
-            src={user?.imageUrl}
-            width={64}
-            height={64}
-            alt='preview image'
-            objectFit='cover'
-            className='rounded-[10px]'
-          />
-        </div>
+        <Notification allMessage={allMessage} />
+        <UserLogo user={user} className={styles.userImg} />
       </div>
-      <UserPanelModalWindow
-        visibleModal={visibleModal}
-        setVisibleModal={setVisibleModal}
-        user={user}
-        ref={ref}
-      />
-      <NotificationModalWindow
-        visibleModal={visibleNotification}
-        setVisibleModal={setVisibleNotification}
-        message={allMessage}
-        ref={refNotification}
-      />
     </header>
   );
 };
--- app/core/presenter/Layout/Header/UserPanelModalWindow/UserPanelModalWindow.props.ts
+++
@@ -1,9 +0,0 @@
-
-import { IUser } from '@/user/model/user.types'
-import { ButtonHTMLAttributes, DetailedHTMLProps, Dispatch, ReactNode, SetStateAction } from "react"
-
-export type UserPanelModalWindowProps = DetailedHTMLProps<ButtonHTMLAttributes<HTMLDivElement>, HTMLDivElement> & {
-    visibleModal: boolean
-    setVisibleModal: Dispatch<SetStateAction<boolean>>
-    user: IUser | undefined
-}
