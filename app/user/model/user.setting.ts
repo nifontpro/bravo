a40eddc8e035fcbdc08e0d2c767cf89f19aaf336
Nifont <nifontbus@yandex.ru>
2023-01-24 11:37:14 +0300

    +save settings

---
+++ app/user/model/user.setting.ts
@@ -0,0 +1,9 @@
+export interface IUserSaveSettingRequest {
+	userId: string
+	showOnboarding: boolean // true, если просмотрено
+	pageOnboarding: number // Номер последней просмотренной страницы
+}
+
+export interface IUserSetting extends IUserSaveSettingRequest {
+	found: boolean // Признак, найдена ли сохраненная запись с настройками (false - если не было сохранения)
+}
