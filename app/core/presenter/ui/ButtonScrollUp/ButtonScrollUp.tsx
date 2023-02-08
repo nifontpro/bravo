6f1e97d03e43e9a621205ad61987f8e8f216ba21
Zykov Artem <skaterpunisher@gmail.com>
2023-02-08 10:52:11 +0300

    fix

--- app/core/presenter/ui/ButtonScrollUp/ButtonScrollUp.tsx
+++ app/core/presenter/ui/ButtonScrollUp/ButtonScrollUp.tsx
@@ -4,24 +4,48 @@ import UpIcon from '@/core/presenter/images/up.svg';
 import { ButtonScrollUp } from './ButtonScrollUp.props';
 import Button from '../Button/Button';
 import { useScrollY } from '@/core/hooks/useScrollY';
+import { useEffect, useState } from 'react';
 
 const ButtonScrollUp = ({
   className,
   ...props
 }: ButtonScrollUp): JSX.Element => {
-  const y = useScrollY()
+  const y = useScrollY();
+  const [scroll, setScroll] = useState<number>(0);
 
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth'
-    })
-  }
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    document.addEventListener('scroll', scrollHandler);
+    return function () {
+      document.removeEventListener('scroll', scrollHandler);
+    };
+  }, []);
+
+  const scrollHandler = (e: any) => {
+    setScroll(e.target.documentElement.scrollTop);
+  };
 
   return (
-    <div className={cn(styles.wrapper, className)} {...props} onClick={scrollToTop}>
+    <div
+      className={cn(
+        styles.wrapper,
+        {
+          [styles.hidden]: scroll == 0,
+          [styles.visible]: scroll > 0,
+        },
+        className
+      )}
+      {...props}
+      onClick={scrollToTop}
+    >
       <Button className={styles.buttons} size='m' appearance='blackWhite'>
-        Наверх <UpIcon className={styles.icon}/>
+        Наверх <UpIcon className={styles.icon} />
       </Button>
     </div>
   );
